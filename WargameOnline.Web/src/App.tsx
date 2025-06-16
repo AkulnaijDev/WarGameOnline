f04746c8b2e9fdd64f235c9bfaf11e1e469b5025
Gianluca_Fontana <gianluca.fontana.dev@outlook.it>
2025-06-16 19:50:19 +0200

    Minimum skeleton for vite+react+tailwind

---
+++ WargameOnline.Web/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen space-y-20">
+      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Vite, React, Shadcn-ui minimal starter
+      </h1>
+      <div className="bg-green-400 text-6xl font-extrabold p-10 text-center">
+        Test Tailwind 💥
+      </div>
+
+    </main>
+  );
+}
+
+export default App;
